66d1fd05ccf12c26ac0b8b531f9e044639cf30a9
Joseph bahhadi <josephbhd95@outlook.com>
2021-09-30 12:07:05 +0200

    Initial Commit

---
+++ LIAECommerce/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using LIAECommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LIAECommerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
