0ddf489794f9b2afdd52b39d4bcaa52e07528f89
Joseph bahhadi <josephbhd95@outlook.com>
2021-10-05 15:16:03 +0200

    Added microsoft identity

--- LIAECommerce/Data/DBContext.cs
+++ LIAECommerce/Data/DBContext.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using LIAECommerce.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace LIAECommerce.Data
 {
-    public class DBContext : DbContext
+    public class DBContext : IdentityDbContext
     {
         public DBContext(DbContextOptions<DBContext> options)
             : base(options)
--- LIAECommerce/Models/OrderItem.cs
+++ LIAECommerce/Models/OrderItem.cs
@@ -13,6 +13,8 @@ namespace LIAECommerce.Models
         [ForeignKey("Order")]
         public int OrderId { get; set; }
         public Order Order { get; set; }
+
+        [ForeignKey("Product")]
         public int ProductId { get; set; }
         public Product Product { get; set; }
         public int Quantity { get; set; }
